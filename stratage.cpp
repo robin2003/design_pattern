ad56d35dde2c056154d339cf4939c0690af1e626
robin <Robin_Peng@sigmadesigns.com>
2015-02-13 16:42:10 +0800

    add stratage partern.

---
+++ stratage.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+using namespace std;
+
+class ISurfStratage
+{
+public:
+	virtual void doSurfing(  ) = 0;
+	~ISurfStratage(  ) {
+		
+	}
+};
+
+class WifiSurfStratage : public ISurfStratage
+{
+public:
+	virtual void doSurfing(  )
+	{
+		cout<<"using wifi surfing"<<endl;
+	}
+};
+
+class WiredSurfStratage : public ISurfStratage
+{
+public:
+	virtual void doSurfing(  )
+	{
+		cout<<"using wired surfing"<<endl;
+	}
+};
+
+// internet surf has lot of stragage
+class InternetSurf
+{
+public:
+	InternetSurf( ISurfStratage* p )
+		: m_pstratage( p )
+	{
+		
+	}
+
+	void setStratage( ISurfStratage* p )
+	{
+		m_pstratage = p;
+	}
+	
+	void doSurf(  )
+	{
+		m_pstratage->doSurfing(  );
+	}
+private:
+	ISurfStratage* m_pstratage;
+};
+
+
+int main(  )
+{
+	WifiSurfStratage wifi;
+	WiredSurfStratage wired;
+	// wifi surfing
+	InternetSurf internet( &wifi );
+	internet.doSurf(  );
+
+	// wired surfing
+	internet.setStratage( &wired );
+	internet.doSurf(  );
+
+	return 0;
+}
