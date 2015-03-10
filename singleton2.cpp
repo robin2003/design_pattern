8d9dc569fb2a3c5d7d975a742b335e33831d4c2f
robin <Robin_Peng@sigmadesigns.com>
2015-03-10 10:48:32 +0800

    add singleton for lazy mode

---
+++ singleton2.cpp
@@ -0,0 +1,96 @@
+#include <pthread.h>		
+#include <iostream>
+
+using namespace std;
+
+class Mutex
+{
+public:
+	Mutex(  )
+	{
+		pthread_mutex_init( &m_mutex, NULL );
+	}
+	~Mutex(  )
+	{
+		pthread_mutex_destroy( &m_mutex );
+	}
+
+	void lock(  )
+	{
+		pthread_mutex_lock( &m_mutex );
+	}
+
+	void unlock(  )
+	{
+		pthread_mutex_unlock( &m_mutex );	
+	}
+
+private:
+	pthread_mutex_t m_mutex;
+};
+
+class MutexHelper
+{
+public:
+	MutexHelper( Mutex mutex )
+		:m_mutex( mutex )
+	{
+		m_mutex.lock(  );
+	}
+	
+	~MutexHelper(  )
+	{
+		m_mutex.unlock(  );
+	}
+private:
+	Mutex m_mutex;
+};
+
+class Singlton
+{
+public:
+	static Singlton* getInstance(  )
+	{
+		if( m_instance == NULL )
+		{
+			MutexHelper mh( m_mutex );
+			if( m_instance == NULL )
+			{
+				m_instance = new Singlton(  );
+				return m_instance;
+			}
+		}
+		
+	}
+
+	void doSomeThing(  )
+	{
+		cout<<"do some thing"<<endl;
+	}
+private:
+	Singlton(  )
+	{
+	}
+	
+	~Singlton(  )
+	{
+		cout<<"destroy!"<<endl;
+	}
+	Singlton( Singlton& rhs );
+	Singlton& operator=( Singlton& rhs );
+	
+private:
+	static Singlton* m_instance;
+	static Mutex m_mutex;
+};
+
+Singlton* Singlton::m_instance = NULL;
+Mutex Singlton::m_mutex ;
+
+int main( void )
+{
+	Singlton* instance = Singlton::getInstance(  );
+	instance->doSomeThing(  );
+
+	return 0;
+}
